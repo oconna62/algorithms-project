e38b000ec7fcd10d2cbfe9874c507ea058e86310
Anthony O Connor <oconna62@tcd.ie>
2022-04-05 23:20:07 +0100

    edited UI

--- algorithms-project/UI.java
+++ algorithms-project/UI.java
@@ -4,28 +4,37 @@ import java.io.IOException;
 public class UI {
 
     public static boolean userInterface() { 
+    	Scanner input = new Scanner(System.in);
     	boolean quit = false;
+    	
   	  	while(quit != true) {
-  	  		System.out.print("Choose an option below or enter 4 to quit the program:\n"
+  	  		System.out.print("Choose an option below or enter 4 to end the program:\n"
                 + "type 1 to find the shortest path between two bus stops \n"
                 + "type 2 to find a stop's information  \n"
-                + "type 3 to find all trips with a specific time of arrival sorted by trip ID\n");
-        
-  	  		Scanner input = new Scanner(System.in);
-  	  		int inputValue= input.nextInt();
+                + "type 3 to find all trips with a specific time of arrival sorted by trip ID  \n");
   	  		
-  	  		
-  	  		if (inputValue == 1){
-  	  			// DijkstraSP
+  	  		if(input.hasNextInt()) {
+  	  			int inputValue= input.nextInt();  // add a switch here instead
+  	  			if (inputValue == 1){
+  	  				quit = false;
+  	  			}
+  	  			else if (inputValue == 2) {
+  	  				quit = false;
+  	  			}
+  	  			else if (inputValue == 3) {
+  	  				quit = false;
+  	  			} 
+  	  			else if (inputValue == 4) {
+  	  				return true;
+  	  			}
+  	  			else {
+  	  				System.out.println("Please enter a valid number.");
+  	  			}
+  	  			input.nextLine();
   	  		}
-  	  		else if (inputValue == 2) {
-  	  			// stopInformation
-  	  		}
-  	  		else if (inputValue == 3) {
-  	  			// arrivalTime
-  	  		} 
   	  		else {
-  	  			return true;
+  	  			System.out.println("Please enter a valid number.");  
+  	  			input.nextLine();
   	  		}
   	  	}	
   	  	return quit;
