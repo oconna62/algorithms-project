23e43cbaac4c45dbd41a5df7f98aa22b638d9aa7
Anthony O Connor <oconna62@tcd.ie>
2022-04-06 00:04:56 +0100

    working UI

--- algorithms-project/UI.java
+++ algorithms-project/UI.java
@@ -7,38 +7,44 @@ public class UI {
     	Scanner input = new Scanner(System.in);
     	boolean quit = false;
     	
-  	  	while(quit != true) {
-  	  		System.out.print("Choose an option below or enter 4 to end the program:\n"
-                + "type 1 to find the shortest path between two bus stops \n"
-                + "type 2 to find a stop's information  \n"
-                + "type 3 to find all trips with a specific time of arrival sorted by trip ID  \n");
+  	  	System.out.print("Choose an option below or enter 4 to end the program:\n"
+        + "type 1 to find the shortest path between two bus stop\n"
+        + "type 2 to find a stop's information\n"
+        + "type 3 to find all trips with a specific time of arrival sorted by trip ID\n");
   	  		
-  	  		if(input.hasNextInt()) {
-  	  			int inputValue= input.nextInt();  // add a switch here instead
-  	  			if (inputValue == 1){
+  	  	int inputValue = 0;
+  	  	if(input.hasNextInt()) {
+  	  		inputValue = input.nextInt();
+  	  	}
+  	  	
+  	  	if (inputValue >=1 && inputValue <= 4) {
+  	  		switch (inputValue) {
+  	  			case 1: 
   	  				quit = false;
-  	  			}
-  	  			else if (inputValue == 2) {
+  	  				break;
+
+  	  			case 2:
   	  				quit = false;
-  	  			}
-  	  			else if (inputValue == 3) {
+  	  				break;
+  	  			
+  	  			case 3: 
   	  				quit = false;
-  	  			} 
-  	  			else if (inputValue == 4) {
-  	  				return true;
-  	  			}
-  	  			else {
-  	  				System.out.println("Please enter a valid number.");
-  	  			}
-  	  			input.nextLine();
+  	  				break;
+  	  			
+  	  			case 4:
+  	  				quit = true;
+  	  				break;
   	  		}
-  	  		else {
-  	  			System.out.println("Please enter a valid number.");  
-  	  			input.nextLine();
-  	  		}
-  	  	}	
+  	  	}
+  	  	else {
+  	  		System.out.println("Please enter a valid number.");
+			quit = false;
+  	  	}
+  	  	
   	  	return quit;
-	 }	  
+  	  }	
+
+	  
     
     public static void main(String[] args) throws IOException {
         boolean quit = false;
